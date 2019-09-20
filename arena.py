e07e934a29a259b1e1f96de458cf749698c8087a
inux <inux.steve@gmail.com>
2019-09-20 17:10:22 +0200

    created first arena and rule based / random choice player

---
+++ arena.py
@@ -0,0 +1,33 @@
+import logging
+from jass.base.const import JASS_SCHIEBER_1000
+from jass.arena.arena import Arena
+from jass.arena.trump_selection_players_strategy import TrumpPlayerStrategy
+from jass.arena.play_game_nr_rounds_strategy import PlayNrRoundsStrategy
+from random_choice_player import RandomChoicePlayer
+from rule_based_player import RuleBasedPlayer
+
+
+def main():
+    # Set the global logging level (Set to debug or info to see more messages)
+    logging.basicConfig(level=logging.WARNING)
+
+    # setup the arena
+    arena = Arena(jass_type=JASS_SCHIEBER_1000,
+                  trump_strategy=TrumpPlayerStrategy(),
+                  play_game_strategy=PlayNrRoundsStrategy(4))
+    random_choice_player = RandomChoicePlayer()
+    rule_based_player = RuleBasedPlayer()
+
+    arena.set_players(random_choice_player, rule_based_player, random_choice_player, rule_based_player)
+    arena.nr_games_to_play = 1
+    print('Playing {} games'.format(arena.nr_games_to_play))
+    arena.play_all_games()
+    total_games = arena.nr_wins_team_0 + arena.nr_wins_team_1 + arena.nr_draws
+    print('Wins Team 0: {} ({:.2f}%)'.format(arena.nr_wins_team_0, arena.nr_wins_team_0 / total_games))
+    print('Wins Team 1: {} ({:.2f}%)'.format(arena.nr_wins_team_1, arena.nr_wins_team_1 / total_games))
+    print('Draws: {} ({:.2f}%)'.format(arena.nr_draws, arena.nr_draws / total_games))
+    print('Delta Points: {}'.format(arena.delta_points))
+
+
+if __name__ == '__main__':
+    main()
---
+++ random_choice_player.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from jass.base.const import color_masks
+from jass.base.player_round import PlayerRound
+from jass.player.player import Player
+
+
+class RandomChoicePlayer(Player):
+    """
+    Sample implementation of a player to play Jass.
+    """
+    def select_trump(self, rnd: PlayerRound) -> int:
+        """
+        Player chooses a trump based on the given round information.
+
+        Args:
+            rnd: current round
+
+        Returns:
+            selected trump
+        """
+        # select the trump with the largest number of cards
+        print(rnd.hand)
+        print(rnd.hand.shape)
+
+        trump = 0
+        max_number_in_color = 0
+        for c in range(4):
+            number_in_color = (rnd.hand * color_masks[c]).sum()
+            if number_in_color > max_number_in_color:
+                max_number_in_color = number_in_color
+                trump = c
+        return trump
+
+    def play_card(self, rnd: PlayerRound) -> int:
+        """
+        Player returns a card to play based on the given round information.
+
+        Args:
+            rnd: current round
+
+        Returns:
+            card to play, int encoded
+        """
+        # play random
+
+        # get the valid cards to play
+        valid_cards = rnd.get_valid_cards()
+
+        # select a random card
+        return np.random.choice(np.flatnonzero(valid_cards))
