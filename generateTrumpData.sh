551434b9851c0f124c6540a2750a891d4f18caa3
inux <inux.steve@gmail.com>
2019-11-25 00:07:30 +0100

    fromScratch option, 80% most played and 20% of best mean as filter

--- generateTrumpData.sh
+++ generateTrumpData.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+#usage: ./generateTrumpData fromScratch #delete old data, generate all the data from scratch
+#       ./generateTrumpData #only run filter and generate csv
 
-BEST_PLAYER_STAT=0.25
-BEST_STD_PER=0.9
-BEST_MEAN_PER=0.3
+ARG=$1
+
+MOST_GAMES_PER=0.8
+BEST_MEAN_PER=0.2
 CURRENT_DIR=$(pwd)
 OUTPUT_DIR_DATA="$CURRENT_DIR/data/trump"
 OUTPUT_DIR_TEMP="${OUTPUT_DIR_DATA}/temp"
@@ -17,55 +20,72 @@ TEMP_FILTERED_VAL="$OUTPUT_DIR_TEMP/filtered_val"
 
 JASSKIT_BIN="$CURRENT_DIR/jass-kit/tools"
 
-# cleanup
-rm  -rf $OUTPUT_DIR_DATA | true
 mkdir -p $OUTPUT_DIR_TEMP $TEMP_TRAIN $TEMP_TEST $TEMP_VAL
-
 # filterd dirs
 mkdir -p $TEMP_FILTERED_TRAIN $TEMP_FILTERED_TEST $TEMP_FILTERED_VAL
 
-# Training data
-for file in $(find jass-data/split/train -type f | sort); do
-  cat $file >> "${OUTPUT_DIR_TEMP}/train.txt"
-done
+# cleanup - only if fromScratch
+if [ "$ARG" == "fromScratch" ] ; then
+    rm  -rf $OUTPUT_DIR_DATA | true
 
-# Test data
-for file in $(find jass-data/split/test -type f | sort); do
-  cat $file >> "${OUTPUT_DIR_TEMP}/test.txt"
-done
+    # Training data
+    for file in $(find jass-data/split/train -type f | sort); do
+      cat $file >> "${OUTPUT_DIR_TEMP}/train.txt"
+    done
 
-# Validation set (Do not use this for train / validate your model)
-for file in $(find jass-data/split/val -type f | sort); do
-  cat $file >> "${OUTPUT_DIR_TEMP}/val.txt"
-done
+    # Test data
+    for file in $(find jass-data/split/test -type f | sort); do
+      cat $file >> "${OUTPUT_DIR_TEMP}/test.txt"
+    done
+
+    # Validation set (Do not use this for train / validate your model)
+    for file in $(find jass-data/split/val -type f | sort); do
+      cat $file >> "${OUTPUT_DIR_TEMP}/val.txt"
+    done
+
+    # convert to playerround
+    cd $JASSKIT_BIN
+    python convert_rounds_to_player_rounds.py --trump --output player_rnd_trump_train --output_dir $TEMP_TRAIN ${OUTPUT_DIR_TEMP}/train.txt
+    python convert_rounds_to_player_rounds.py --trump --output player_rnd_trump_test --output_dir $TEMP_TEST ${OUTPUT_DIR_TEMP}/test.txt
+    python convert_rounds_to_player_rounds.py --trump --output player_rnd_trump_val --output_dir $TEMP_VAL ${OUTPUT_DIR_TEMP}/val.txt
+
+    # merge playerround to one big file
+    # Training data
+    for file in $(find $TEMP_TRAIN -type f | sort); do
+    cat $file >> "${OUTPUT_DIR_TEMP}/train_rounds_merged.txt"
+    done
+
+    # Test data
+    for file in $(find $TEMP_TEST -type f | sort); do
+    cat $file >> "${OUTPUT_DIR_TEMP}/test_rounds_merged.txt"
+    done
+
+    # Validation set (Do not use this for train / validate your model)
+    for file in $(find $TEMP_VAL -type f | sort); do
+    cat $file >> "${OUTPUT_DIR_TEMP}/val_rounds_merged.txt"
+    done
+fi
+
+if [ ! -f ${OUTPUT_DIR_TEMP}/train_rounds_merged.txt ]; then
+    echo "train_rounds_merged.txt does not exists run ./generateTrumpData.sh fromScratch"
+    exit 1
+fi
+if [ ! -f ${OUTPUT_DIR_TEMP}/test_rounds_merged.txt ]; then
+    echo "test_rounds_merged.txt does not exists run ./generateTrumpData.sh fromScratch"
+    exit 1
+fi
+if [ ! -f ${OUTPUT_DIR_TEMP}/val_rounds_merged.txt ]; then
+    echo "val_rounds_merged.txt does not exists run ./generateTrumpData.sh fromScratch"
+    exit 1
+fi
 
 
-# convert to playerround
 cd $JASSKIT_BIN
-python convert_rounds_to_player_rounds.py --trump --output player_rnd_trump_train --output_dir $TEMP_TRAIN ${OUTPUT_DIR_TEMP}/train.txt
-python convert_rounds_to_player_rounds.py --trump --output player_rnd_trump_test --output_dir $TEMP_TEST ${OUTPUT_DIR_TEMP}/test.txt
-python convert_rounds_to_player_rounds.py --trump --output player_rnd_trump_val --output_dir $TEMP_VAL ${OUTPUT_DIR_TEMP}/val.txt
-
-# merge playerround to one big file
-# Training data
-for file in $(find $TEMP_TRAIN -type f | sort); do
-  cat $file >> "${OUTPUT_DIR_TEMP}/train_rounds_merged.txt"
-done
-
-# Test data
-for file in $(find $TEMP_TEST -type f | sort); do
-  cat $file >> "${OUTPUT_DIR_TEMP}/test_rounds_merged.txt"
-done
-
-# Validation set (Do not use this for train / validate your model)
-for file in $(find $TEMP_VAL -type f | sort); do
-  cat $file >> "${OUTPUT_DIR_TEMP}/val_rounds_merged.txt"
-done
 
 # filter the best players form the merged rounds
-python filter_player_rounds.py --std_best_perc $BEST_STD_PER --mean_best_perc $BEST_MEAN_PER --output filtered_trump_train --output_dir $TEMP_FILTERED_TRAIN --stat $CURRENT_DIR/jass-data/stat/player_all_stat.json ${OUTPUT_DIR_TEMP}/train_rounds_merged.txt
-python filter_player_rounds.py --std_best_perc $BEST_STD_PER --mean_best_perc $BEST_MEAN_PER --output filtered_trump_test --output_dir $TEMP_FILTERED_TEST --stat $CURRENT_DIR/jass-data/stat/player_all_stat.json ${OUTPUT_DIR_TEMP}/test_rounds_merged.txt
-python filter_player_rounds.py --std_best_perc $BEST_STD_PER --mean_best_perc $BEST_MEAN_PER --output filtered_trump_val --output_dir $TEMP_FILTERED_VAL --stat $CURRENT_DIR/jass-data/stat/player_all_stat.json ${OUTPUT_DIR_TEMP}/val_rounds_merged.txt
+python filter_player_rounds.py --played_games_most_perc $MOST_GAMES_PER --mean_best_perc $BEST_MEAN_PER --output filtered_trump_train --output_dir $TEMP_FILTERED_TRAIN --stat $CURRENT_DIR/jass-data/stat/player_all_stat.json ${OUTPUT_DIR_TEMP}/train_rounds_merged.txt
+python filter_player_rounds.py --played_games_most_perc $MOST_GAMES_PER --mean_best_perc $BEST_MEAN_PER --output filtered_trump_test --output_dir $TEMP_FILTERED_TEST --stat $CURRENT_DIR/jass-data/stat/player_all_stat.json ${OUTPUT_DIR_TEMP}/test_rounds_merged.txt
+python filter_player_rounds.py --played_games_most_perc $MOST_GAMES_PER --mean_best_perc $BEST_MEAN_PER --output filtered_trump_val --output_dir $TEMP_FILTERED_VAL --stat $CURRENT_DIR/jass-data/stat/player_all_stat.json ${OUTPUT_DIR_TEMP}/val_rounds_merged.txt
 
 # merge filterd files to one huge file
 # Training data
