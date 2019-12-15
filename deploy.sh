b8d4b4c440567780593edad738cd65d7ab94e666
Remo Schwarzentruber <Remo.Schwarzentruber@stud.hslu.ch>
2019-12-15 15:52:29 +0100

    add requirements and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 #copy everything to tschinux but not files/folders starting with a dot (e.g .git)
-scp -r [!.]* tschinux@shoemaker.uberspace.de:/home/tschinux/dl4g/
+deploy_user_host=tschinux@shoemaker.uberspace.de
+
+
+# relative to home directory unless preceded by a slash,
+# WARNING: watch out not to delete the whole thing when using:
+#   deploy.sh complete
+deployment_folder="dl4g"
+
+d="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+if [ "x${1:-}" == "xcomplete" ]; then
+    echo "Deleting previous deployment:"
+    ssh "$deploy_user_host" "test -n "\$HOME" && rm -rv '$deployment_folder'"
+fi
+rsync -avz --exclude-from="$d"/.gitignore --exclude .git --exclude jass-data "$d/" "$deploy_user_host:$deployment_folder/"
+echo "INFO: finished."
